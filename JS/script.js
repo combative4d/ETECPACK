e80db4d2dd043617eddc0af785499886fceb115f
COMB4TIVE <114819878+combative4d@users.noreply.github.com>
2023-08-29 17:15:39 -0300

    Add files via upload
    Arquivos de programação em HTML que tenho que guardar aqui. Vai ver no futuro vai ser útil.

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+var curso=prompt("Digite ai teu curso")
+var nome=prompt("Nome do desempregado","davi e homexesual")
+var N1=parseFloat(prompt("Nota 1"))
+var N2=parseFloat(prompt("Nota 2"))
+var Media=((N1+N2)/2)
+var ql="\n"
+alert(nome+ql+"Media Anual"+media)
